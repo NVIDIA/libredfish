55a52d2c3844bcce9e92ff1f5ae843699703f888
David DeJong <ddejong@nvidia.com>
2025-12-11 03:50:54 +0000

    feat: Retrieve positional info from GB200 chassis info
    Signed-off-by: David DeJong <ddejong@nvidia.com>

--- src/model/oem/nvidia_dpu.rs
+++ src/model/oem/nvidia_dpu.rs
@@ -66,14 +66,20 @@ impl fmt::Display for HostPrivilegeLevel {
 /// This OEM specific extension is mainly applicable for querying chassis information for the ERoT subsystem
 /// odata_type is always present regardless of the subsystem we are querying for (Bluefield_BMC, Bluefield_ERoT, or Card1)
 /// the remaining attributes are only present when querying the Bluefield_ERoT
+/// Due to the indistinguishable names, this is also used for non-DPU reporting NVidia, such as GB200; comments describe
+/// what platforms it may be reasonable expected on.
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "PascalCase")]
 pub struct Chassis {
     #[serde(rename = "@odata.type")]
     pub odata_type: String,
-    pub automatic_background_copy_enabled: Option<bool>,
-    pub background_copy_status: Option<BackgroundCopyStatus>,
-    pub inband_update_policy_enabled: Option<bool>,
+    pub automatic_background_copy_enabled: Option<bool>, // DPU
+    pub background_copy_status: Option<BackgroundCopyStatus>, // DPU
+    pub inband_update_policy_enabled: Option<bool>,      // DPU
+    pub chassis_physical_slot_number: Option<i32>,       // GB200
+    pub compute_tray_index: Option<i32>,                 // GB200
+    pub topology_id: Option<i32>,                        // GB200
+    pub revision_id: Option<i32>,                        // GB200
 }
 
 #[derive(Debug, Serialize, Deserialize, Copy, Clone, Eq, PartialEq)]
